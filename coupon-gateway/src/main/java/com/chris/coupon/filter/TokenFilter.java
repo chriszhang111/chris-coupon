32d5a9e55d29e300981f5b07dcac1482315d4650
chris <869415122@qq.com>
2020-02-23 20:41:29 -0500

    add token filter

---
+++ coupon-gateway/src/main/java/com/chris/coupon/filter/TokenFilter.java
@@ -0,0 +1,30 @@
+package com.chris.coupon.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class TokenFilter extends AbstractPreZuulFilter {
+
+    @Override
+    protected Object cRun() {
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s request to %s"),
+                request.getMethod(),
+                request.getRequestURL().toString());
+        Object token = request.getParameter("token");
+        if(token == null){
+            log.error("error: token is empty");
+            return fail(401, "error: token is empty");
+        }
+        return success();
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+}
