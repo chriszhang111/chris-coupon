6871cb8c1646820b221d116e8de9aa71e5a8129d
chris <869415122@qq.com>
2020-02-23 21:20:53 -0500

    common module init

---
+++ chris-coupon-service/coupon-common/src/main/java/com/chris/coupon/conf/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.chris.coupon.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/*
+* 定制Http消息转换器
+* target：Java对象 -> Http请求
+* */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
---
+++ chris-coupon-service/coupon-common/src/main/java/com/chris/coupon/tmp.java
@@ -0,0 +1,4 @@
+package com.chris.coupon;
+
+public class tmp {
+}
