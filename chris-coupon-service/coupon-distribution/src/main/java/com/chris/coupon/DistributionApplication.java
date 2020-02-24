b27c7dc5d5b446fd0185e1bf578f8f36719ad0c6
chris <869415122@qq.com>
2020-02-24 18:12:56 -0500

    distribution module added

---
+++ chris-coupon-service/coupon-distribution/src/main/java/com/chris/coupon/DistributionApplication.java
@@ -0,0 +1,30 @@
+package com.chris.coupon;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
+
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class DistributionApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionApplication.class, args);
+    }
+}
