077e5622505226bd2c676e113b97a12272d0ed00
chris <869415122@qq.com>
2020-02-25 17:52:03 -0500

    kafka service added

--- chris-coupon-service/coupon-distribution/src/main/java/com/chris/coupon/service/impl/KafkaServiceImpl.java
+++ chris-coupon-service/coupon-distribution/src/main/java/com/chris/coupon/service/impl/KafkaServiceImpl.java
@@ -1,18 +1,26 @@
 package com.chris.coupon.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.chris.coupon.constant.Constant;
+import com.chris.coupon.constant.CouponStatus;
 import com.chris.coupon.dao.CouponDao;
+import com.chris.coupon.entity.Coupon;
 import com.chris.coupon.service.IKafkaService;
+import com.chris.coupon.vo.CouponKafkaMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
+import org.springframework.stereotype.Component;
+import java.util.List;
 import java.util.Optional;
 
+/**
+ * 将cache中的状态变化同步到mysql中
+ * */
 @Slf4j
-@Service
+@Component
 public class KafkaServiceImpl implements IKafkaService{
 
     private final CouponDao couponDao;
@@ -29,7 +37,53 @@ public class KafkaServiceImpl implements IKafkaService{
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
         if(kafkaMessage.isPresent()){
             Object message = kafkaMessage.get();
+            CouponKafkaMessage couponInfo = JSON.parseObject(message.toString(), CouponKafkaMessage.class);
+            log.info("Receive CouponKafkaMessage: {}", message.toString());
+
+            CouponStatus status = CouponStatus.of(couponInfo.getStatus());
+            switch (status){
+                case USABLE:
+                    break;
+                case USED:
+                    processUsedCoupons(couponInfo, status);
+                    break;
+                case EXPIRED:
+                    processExpiredCoupons(couponInfo, status);
+                    break;
+            }
+
 
         }
     }
+
+    private void processUsedCoupons(CouponKafkaMessage kafkaMessage, CouponStatus status) {
+        processCouponsByStatus(kafkaMessage, status);
+    }
+
+    private void processExpiredCoupons(CouponKafkaMessage kafkaMessage,
+                                       CouponStatus status) {
+        // TODO 给用户发送推送
+        processCouponsByStatus(kafkaMessage, status);
+    }
+
+    private void processCouponsByStatus(CouponKafkaMessage kafkaMessage,
+                                        CouponStatus status) {
+        List<Coupon> coupons = couponDao.findAllById(
+                kafkaMessage.getIds()
+        );
+
+        if (CollectionUtils.isEmpty(coupons)
+                || coupons.size() != kafkaMessage.getIds().size()) {
+            log.error("Can Not Find Right Coupon Info: {}",
+                    JSON.toJSONString(kafkaMessage));
+            // TODO 发送邮件
+            return;
+        }
+
+        coupons.forEach(c -> c.setStatus(status));
+        log.info("CouponKafkaMessage Op Coupon Count: {}",
+                couponDao.saveAll(coupons).size());
+    }
+
+
 }
